b771d85a7e403f5773b821ed446d79621ae904f5
Sergi Uceda <sergi.uceda@wefoxgroup.com>
2020-06-24 21:53:05 +0200

    Test package version action

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
+import * as fs from 'fs'
 import * as core from '@actions/core'
 import {wait} from './wait'
+
+const getPackageVersion = (): string => {
+  const data = JSON.parse(fs.readFileSync('package.json', {encoding: 'utf-8'}))
+
+  return data.version
+}
 
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`)
+
+    core.debug(`package version: ${getPackageVersion()}`)
 
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
